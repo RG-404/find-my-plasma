55f49ee48eaf7e25be5b7b86558eee4453687eb5
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-05-03 01:46:33 +0530

    configure favicon site.webmanifest

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,6 @@ class MyDocument extends Document {
             httpEquiv="Content-Type"
             content="text/html; charset=ISO-8859-1"
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main />

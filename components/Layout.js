b4ed1668a1a4de408f1adcd22c1f05d1baf9162a
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-05-03 16:55:48 +0530

    remove beta tag

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,9 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import BetaTag from "./BetaTag";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <BetaTag />
       <NavBar />
       {children}
       <Footer />

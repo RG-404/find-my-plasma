d12b02617229bc084d34e49bf7545909ab6a568c
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-05-03 10:15:31 +0530

    fix inaccessible public key
        fix typo in env variable name
        change NEXT_PUBILC_ to NEXT_PUBLIC_
    
        closes #1

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,22 +1,12 @@
 import firebase from "firebase/app";
-// import config from "../firebase.config";
-
-// const config = {
-//   apiKey: process.env.NEXT_PUBILC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBILC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBILC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBILC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBILC_FIREBASE_MESSAGE_SENDER_ID,
-//   appId: process.env.NEXT_PUBILC_FIREBASE_APP_ID,
-// };
 
 const config = {
-  apiKey: "AIzaSyBM4vK778toP83COqKQWF9loxiY7mqPga8",
-  authDomain: "find-my-plasma.firebaseapp.com",
-  projectId: "find-my-plasma",
-  storageBucket: "find-my-plasma.appspot.com",
-  messagingSenderId: "61512897111",
-  appId: "1:61512897111:web:ea2e5ed54420240ee2c912",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const initFirebase = () => {

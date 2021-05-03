1cfde74670158d21606254c016b151be4424e8f8
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-05-03 10:55:56 +0530

    fix analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,37 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Head from "next/head";
+import initFirebase from "../utils/firebase";
 import firebase from "firebase/app";
 import "firebase/analytics";
+import { useRouter } from "next/router";
 
-firebase.analytics();
+initFirebase();
 
 function MyApp({ Component, pageProps }) {
+  const routers = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      firebase.analytics();
+
+      const logEvent = (url) => {
+        firebase.analytics().setCurrentScreen(url);
+        firebase.analytics().logEvent("screen_view");
+      };
+
+      routers.events.on("routeChangeComplete", logEvent);
+      //For First Page
+      logEvent(window.location.pathname);
+
+      //Remvove Event Listener after un-mount
+      return () => {
+        routers.events.off("routeChangeComplete", logEvent);
+      };
+    }
+  }, []);
+
   return (
     <Fragment>
       <Head>
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/analytics";
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -7,10 +8,10 @@ const config = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const initFirebase = () => {
-  console.log(config);
   if (!firebase.apps.length) {
     firebase.initializeApp(config);
   }
